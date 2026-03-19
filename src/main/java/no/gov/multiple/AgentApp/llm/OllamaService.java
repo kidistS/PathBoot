2040383077a3620da66d07f82d7077d7442a8fcd
KidistSG <kidseifu11@gmaiil.com>
2026-03-20 00:47:08 +0100

    Ollama service implmented

---
+++ src/main/java/no/gov/multiple/AgentApp/llm/OllamaService.java
@@ -0,0 +1,17 @@
+package no.gov.multiple.AgentApp.llm;
+import org.springframework.ai.chat.client.ChatClient;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OllamaService {
+    private final ChatClient chatClient;
+
+    public OllamaService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    public String ask(String prompt) {
+        return chatClient.prompt(prompt).call().content();
+    }
+}
