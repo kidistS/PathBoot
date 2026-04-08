758611b8a8f2af831ad8f51e78ea0f9f2ec0d7bc
KidistSG <kidseifu11@gmaiil.com>
2026-04-08 13:49:16 +0200

    code recactored

--- src/main/java/no/gov/multiple/AgentApp/GovAIAssistantApp.java
+++ src/main/java/no/gov/multiple/AgentApp/GovAIAssistantApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgentAppApplication {
+public class GovAIAssistantApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AgentAppApplication.class, args);
+		SpringApplication.run(GovAIAssistantApp.class, args);
 	}
 
 }
